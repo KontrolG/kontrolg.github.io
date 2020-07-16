d9d3da2a80e2503b95a57c7c1d6fbdf883f9886c
German Solano <kontrolg1239@gmail.com>
2020-07-16 09:19:04 -0400

    Cambios menores y favicon añadido

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,44 +1,25 @@
-const goToSectionInHash = event => {
-  const sectionSelector = getDocumentHash();
-  if (sectionSelector) {
-    scrollToSection(sectionSelector);
-  }
-};
+var toggleNavigationElement = document.querySelector(".toggle-navigation");
 
-const getDocumentHash = () => location.hash;
-
-const scrollToSection = sectionSelector => {
-  const sectionElement = getSectionElement(sectionSelector);
-  const scrollOptions = getScrollOptionsFrom(sectionElement);
-  scrollTo(scrollOptions);
-};
-
-const getSectionElement = sectionSelector =>
-  document.querySelector(sectionSelector);
-
-const getScrollOptionsFrom = ({ offsetTop, offsetLeft }) => ({
-  top: offsetTop,
-  left: offsetLeft,
-  behavior: "smooth"
-});
-
-window.addEventListener("load", goToSectionInHash);
-
-const toggleNavigationElement = document.querySelector(".toggle-navigation");
-
-const toggleOpen = event => {
+function preventHashChange(event) {
   event.preventDefault();
-  const { target } = event;
-  const parentButtonElement = target.closest("a");
-  const siblingElement = parentButtonElement.nextElementSibling;
+}
+
+function toggleOpen(event) {
+  var target = event.target;
+  toggleSiblingElement(target);
+  toggleParentNavigation(target);
+}
+
+function toggleSiblingElement(target) {
+  var parentButtonElement = target.closest("a");
+  var siblingElement = parentButtonElement.nextElementSibling;
   siblingElement.classList.toggle("open");
-};
+}
 
-const toggleNavigationBackground = event => {
-  const { target } = event;
-  const parentNavigation = target.closest("nav");
+function toggleParentNavigation(target) {
+  var parentNavigation = target.closest("nav");
   parentNavigation.classList.toggle("main-navigation-opened");
-};
+}
 
+toggleNavigationElement.addEventListener("click", preventHashChange);
 toggleNavigationElement.addEventListener("click", toggleOpen);
-toggleNavigationElement.addEventListener("click", toggleNavigationBackground);
