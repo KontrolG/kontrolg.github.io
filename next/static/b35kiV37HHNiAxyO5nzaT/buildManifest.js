6104531ba2ed7723d97642a2f39519b11bbc3bca
German Solano <kontrolg1239@gmail.com>
2021-12-06 10:13:34 -0400

    Fix files with underscore prefix

---
+++ next/static/b35kiV37HHNiAxyO5nzaT/buildManifest.js
@@ -0,0 +1,20 @@
+(self.__BUILD_MANIFEST = (function (e, s, c) {
+  return {
+    __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+    "/": [
+      e,
+      s,
+      "static/chunks/44-75fab73e03942d4c.js",
+      c,
+      "static/chunks/pages/index-3b4fc5eab785e216.js"
+    ],
+    "/404": [e, s, c, "static/chunks/pages/404-d3289959f2d82343.js"],
+    "/_error": ["static/chunks/pages/error-2280fa386d040b66.js"],
+    sortedPages: ["/", "/404", "/_app", "/_error"]
+  };
+})(
+  "static/chunks/1bfc9850-cf0beaeba26daaf1.js",
+  "static/chunks/634-2f6620fa1f7a1d5f.js",
+  "static/chunks/486-e120dc8f8d962a10.js"
+)),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
